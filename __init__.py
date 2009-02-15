42fd7a23c049913367dab6b850c2fca120654bcd
Bradley Wright <brad@intranation.com>
2009-02-15 16:05:00 +0000

    Style tidy ups and fix a bug in default URL routing

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ __all__ = [
 class HttpError(Exception):
     pass
 
+
 class HttpRequest(object):
     """A single HTTP request"""
     def __init__(self, environ):
@@ -31,51 +32,23 @@ class HttpRequest(object):
         self.REQUEST = self.GET.copy()
         self.REQUEST.update(self.POST)
 
+
 class HttpResponse(object):
     """A single HTTP response"""
     status_codes = {
-        100: "Continue", 
-        101: "Switching Protocols", 
-        200: "OK", 
-        201: "Created", 
-        202: "Accepted", 
-        203: "Non-Authoritative Information", 
-        204: "No Content", 
-        205: "Reset Content", 
-        206: "Partial Content", 
-        300: "Multiple Choices", 
-        301: "Moved Permanently", 
-        302: "Found", 
-        303: "See Other", 
-        304: "Not Modified", 
-        305: "Use Proxy", 
-        307: "Temporary Redirect", 
-        400: "Bad Request", 
-        401: "Unauthorized", 
-        402: "Payment Required", 
-        403: "Forbidden", 
-        404: "Not Found", 
-        405: "Method Not Allowed", 
-        406: "Not Acceptable",
-        407: "Proxy Authentication Required", 
-        408: "Request Time-out", 
-        409: "Conflict", 
-        410: "Gone", 
-        411: "Length Required", 
-        412: "Precondition Failed", 
-        413: "Request Entity Too Large", 
-        414: "Request-URI Too Large", 
-        415: "Unsupported Media Type", 
-        416: "Requested range not satisfiable", 
-        417: "Expectation Failed", 
-        500: "Internal Server Error", 
-        501: "Not Implemented", 
-        502: "Bad Gateway", 
-        503: "Service Unavailable", 
-        504: "Gateway Time-out", 
-        505: "HTTP Version not supported",
-        }
-    
+        200: "OK",
+        201: "Created",
+        202: "Accepted",
+        301: "Moved Permanently",
+        302: "Found",
+        303: "See Other",
+        304: "Not Modified",
+        403: "Forbidden",
+        404: "Not Found",
+        405: "Method Not Allowed",
+        410: "Gone",
+        500: "Internal Server Error",
+    }
     
     def __init__(self, content='', headers={}, status_code=200):
         self.status_code = status_code
@@ -167,8 +140,9 @@ class WebApplication(object):
 if __name__ == '__main__':
     """run the basic server"""
     routes = (
-        ('/', 'homepage'),
+        (r'^/$', 'homepage'),
     )
+    
     def homepage(request):
         return HttpResponse('hello world!')
     
