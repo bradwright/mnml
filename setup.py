b46119ada62fbcb4791cd8ce210e34a43564df5b
Bradley Wright <brad@intranation.com>
2009-02-15 19:06:26 +0000

    Add setup_tools cruft

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name         = 'MNML',
+      description  = 'A very lightweight WSGI Python web framework',
+      author       = 'Bradley Wright',
+      author_email = 'me@bradleywright.name',
+      url          = 'http://github.com/bradleywright/mnml',
+      version      = '0.1',
+      py_modules   = ['mnml'],
+     )
